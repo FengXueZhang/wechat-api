5de7c92ce3448914013557eb3164cb1b60b5b4d1
cannon <mr.zhangxuefeng@gmail.com>
2017-07-18 10:07:57 +0800

    first commit

--- src/API/ServiceApi.php
+++ src/API/ServiceApi.php
@@ -20,11 +20,11 @@ class ServiceApi extends BaseApi
      *
      * @param string $redirectUri 协议的回调地址
      * @param string $state       可携带的参数, 选填.
-     * @param string $usertype    redirect_uri支持登录的类型,默认是admin.
+     * @param string $agentid     授权方的网页应用ID.
      *
      * @return string 协议地址
      */
-    public function createOAuthUrl($redirectUri, $state = '', $usertype = 'member')
+    public function createOAuthUrl($redirectUri, $state = '', $agentid)
     {
         if (!$redirectUri) {
             $this->setError('参数错误!');
@@ -33,14 +33,13 @@ class ServiceApi extends BaseApi
         }
 
         $host = isset($_SERVER['HTTP_HOST']) ? 'http://' . $_SERVER['HTTP_HOST'] : '';
-        $api = 'https://qy.weixin.qq.com/cgi-bin/loginpage';
+        $api = 'https://open.work.weixin.qq.com/wwopen/sso/qrConnect';
 
         $state = $state ? $state = base64_encode($state) : '';
 
         $url = array();
-        $url['corp_id'] = Api::getCorpId();
-        $url['redirect_uri'] = $host . $redirectUri;
-        $url['usertype'] = $usertype;
+        $url['appid'] = Api::getCorpId();
+        $url['agentid'] = $host . $redirectUri;
         $url['state'] = $state;
 
         $url = http_build_query($url);
@@ -81,71 +80,7 @@ class ServiceApi extends BaseApi
      */
     public function receive()
     {
-        $code = I('get.auth_code', false, 'trim');
-
-        if (!$code) {
-            $this->setError('非法参数');
-
-            return false;
-        }
-
-        $res = $this->getIdByCode($code);
-        if (false == $res || !isset($res['user_info']) || !$res['user_info']) {
-            $this->setError('对不起,您尚不是本站用户.');
-
-            return false;
-        }
-
-        $arr = array();
-        $arr['userid'] = $res['user_info']['userid'];
-        $arr['state'] = I('get.state', '', 'trim,base64_decode');
-        $arr['auth_code'] = $code;
-
-        return $arr;
+        return Api::factory("User")->receive();
     }
 
-    /**
-     * 根据协议换回的code换取用户的userid.
-     *
-     * @author XueFeng
-     *
-     * @date   2016-04-21
-     *
-     * @param string $code 协议换回的code
-     *
-     * @return string userid
-     */
-    public function getIdByCode($code)
-    {
-        if (false == $code) {
-            $this->setError('参数错误');
-
-            return false;
-        }
-
-        $node = 'get_login_info';
-
-        $queryStr = array(
-            'auth_code' => $code,
-        );
-
-        return $this->_post($node, $queryStr);
-    }
-
-    /**
-     * 根据用户ID获取用户信息.
-     *
-     * @author XueFeng
-     *
-     * @date   2016-04-21
-     *
-     * @param string $userId 用户在微信端的userid.
-     *
-     * @return array 用户信息
-     */
-    public function getInfoById($userId)
-    {
-        return Api::factory("User")->getInfoById($userId);
-        //return $this->_get($node, $queryStr);
-    }
 }
